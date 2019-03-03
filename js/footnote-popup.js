2996e55acb4cfa5240b44d0d7fef75682d2b603c
hoon <hoonti06@gmail.com>
2019-03-03 13:24:22 +0900

    rename footnotes to footnote-popup

--- js/footnote-popup.js
+++ js/footnote-popup.js
@@ -1,76 +1,69 @@
-function getFootnoteContent(index) {
-    const id = "fn:" + index;
-    const fn = document.getElementById(id);
-    return fn.innerHTML.trim();
-};
+let wrapper     = document.getElementById("popup-wrapper"); 
+let content     = document.getElementById("popup-content"); 
 
-function footnotePopup(showIndex, showCloseBtn) {
-    const popupWrapper = document.querySelector("#popup-wrapper");
-    
-    // Set whether to display index and/or close button. Default is true for both
-    if (showIndex === undefined) {
-        showIndex = true;
-    } 
-    
-    if (showCloseBtn === undefined) {
-        showCloseBtn = true;
-    }
 
-    // Create main container that will hold footnote content
-    const popupContent = popupWrapper.appendChild(document.createElement("div"));
-    popupContent.id = "popup-content";
+function gatherFootnotes() {
+    // Counts generated footnotes. Hugo will create ordered list at the end of each post 
+    // with each footnote as item.
+    let fnCount = document.getElementById("fn:1").parentNode.children.length;
 
-    let popupIndex = null;
-    if (showIndex) {
-        popupIndex = popupWrapper.insertBefore(document.createElement("div"), popupContent);
-        popupIndex.id = "popup-index";
-    }
-
-    let popupCloseButton = null;
-    if (showCloseBtn) {
-        popupCloseButton = popupWrapper.appendChild(document.createElement("div"));
-        popupCloseButton.innerHTML = "[x]";
-        popupCloseButton.id = "popup-close";
-    }
-
-    // Remove redundant [return] links from footnote list (optional)
-    const fnReturns = document.querySelectorAll("a.footnote-return");
-    fnReturns.forEach(function(fnReturn) {
-        const parent = fnReturn.parentNode;
-        parent.removeChild(fnReturn);
-    });
-
-    const fnRefs = document.querySelectorAll("sup.footnote-ref");
-    fnRefs.forEach(function(fnRef) {
-        fnRef.addEventListener("click", handler("refs", fnRef));
-    });
-
-    window.addEventListener("scroll", handler("close"));
-
-    if (showCloseBtn) {
-        popupCloseButton.addEventListener("click", handler("close"));
-    }
-
-    function handler(type, node) {
-        return function(event) {
-            if (type === "close") {
-                popupWrapper.style.display = "none";
-            }
-
-            if (type === "refs") {
-                event.preventDefault();
-
-                const index = node.id.substring(6, 7);
-
-                if (showIndex) {
-                    popupIndex.innerHTML = index + ".";
-                }
-
-                popupContent.innerHTML = getFootnoteContent(index);
-                popupWrapper.style.display = "flex";
-            }
-        };
+    // Add every footnote into fnList ordered by their index.
+    let fnList = []; 
+    for (let x=1; x <= fnCount; x++) {
+        let footnote = document.getElementById(`fn:${x}`);
+        fnList.push(footnote);
     };
+
+    return fnList;
+}
+
+let fnList = gatherFootnotes();
+
+
+function getFootnote(index) {
+    return fnList[index-1].innerHTML.trim();
 };
 
-footnotePopup(true, true);
+
+function closeFootnotePopup() {
+    if (wrapper.style.display !== "none") {
+        wrapper.style.display = "none";
+    }
+}
+
+
+// This will remove return links from footnote content.
+function removeReturnLinks() {
+    let fnReturn = document.getElementsByClassName("footnote-return"); 
+    while (fnReturn.length > 0) {
+        fnReturn[0].parentNode.removeChild(fnReturn[0]);
+    };
+}
+
+
+function footnotePopup() {
+    removeReturnLinks();
+
+    let refList = document.querySelectorAll("sup");
+    for (let x = 0; x < refList.length; x++) {
+        refList[x].addEventListener("click", function(event) {
+            event.preventDefault();    
+            // index.innerHTML = refList[x].id.substring(6,7) + ".";            
+            content.innerHTML = getFootnote(refList[x].id.substring(6,7));
+            wrapper.style.display = "flex";
+
+        });
+    };
+
+    wrapper.addEventListener("click", function(event) {
+        closeFootnotePopup();
+    });
+
+
+    window.addEventListener("scroll", function(event) {
+        closeFootnotePopup();
+    });
+};
+
+footnotePopup();
+
