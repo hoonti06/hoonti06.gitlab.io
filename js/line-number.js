6e4ce80ccf36c4d85e3abd61b1eb1501e5982a6b
hoon <hoonti06@gmail.com>
2019-03-13 01:22:02 +0900

    modify js/line-number.js

--- js/line-number.js
+++ js/line-number.js
@@ -2,13 +2,14 @@ function addLineNumberInCodeBlock() {
 	let pre = document.getElementsByTagName('pre');
 	let pl = pre.length;
 	for (let i = 0; i < pl; i++) {
-		pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
+		pre[i].innerHTML = '<span class="line-number" style="display: block; float: left; margin: 0 1em 0 -1em; border-right: 1px solid #ddd; text-align: right;"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
 		let num = pre[i].innerHTML.split(/\n/).length;
 		for (let j = 0; j < (num - 1); j++) {
 			let line_num = pre[i].getElementsByTagName('span')[0];
-			line_num.innerHTML += '<span>' + (j + 1) + '</span>';
+			line_num.innerHTML += '<span style="display: block;">' + (j + 1) + '</span>';
 		}
 	}
 }
 
 addLineNumberInCodeBlock();
+
