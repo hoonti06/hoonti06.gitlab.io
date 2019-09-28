bd0c4f807ed93888e9e76132015d556d11dbf059
hoon <hoonti06@gmail.com>
2019-09-28 11:50:54 +0900

    build_Docker-Jekyll.sh 수정

--- build_Docker-Jekyll.sh
+++ build_Docker-Jekyll.sh
@@ -1,1 +1,1 @@
-docker run -it --name blog --volume="$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll build -d public
+docker run -it --name build-jekyll --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll build -d public
