8fe3c0196f262eb6ee417fbd59c8297e3ccbe979
hoon <hoonti06@gmail.com>
2019-10-01 14:22:44 +0900

    process가 아직 남아 있으면 삭제하고 실행하도록 수정

--- run_Docker-Jekyll.sh
+++ run_Docker-Jekyll.sh
@@ -1,1 +1,2 @@
-docker run -it --name blog --volume="$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll serve
+docker rm blog
+docker run -it --name blog --volume="$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll serve --watch --trace
