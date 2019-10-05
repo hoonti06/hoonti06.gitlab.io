52b69f27bfb60fcf25a53c6b23774f37c32c8260
hoon <hoonti06@gmail.com>
2019-10-05 11:14:56 +0900

    pandoc으로 convert된 형식에 맞게 footnote pop-up되도록 수정

--- js/footnote-popup.js
+++ js/footnote-popup.js
@@ -3,66 +3,65 @@ let content     = document.getElementById("popup-content");
 
 
 function gatherFootnotes() {
-    // Counts generated footnotes. Hugo will create ordered list at the end of each post 
-    // with each footnote as item.
-    let fnCount = document.getElementById("fn:1").parentNode.children.length;
 
-    // Add every footnote into fnList ordered by their index.
-    let fnList = []; 
-    for (let x=1; x <= fnCount; x++) {
-        let footnote = document.getElementById(`fn:${x}`);
-        fnList.push(footnote);
-    };
+  let fnList = []; 
 
-    return fnList;
+  let i = 1;
+  let footnote;
+  while (footnote = document.getElementById(`fn${i}`))
+  {
+    fnList.push(footnote);
+    i++;
+  }
+
+  return fnList;
 }
 
 let fnList = gatherFootnotes();
 
 
 function getFootnote(index) {
-    return fnList[index-1].innerHTML.trim();
+  return fnList[index-1].innerHTML.trim();
 };
 
 
 function closeFootnotePopup() {
-    if (wrapper.style.display !== "none") {
-        wrapper.style.display = "none";
-    }
+  if (wrapper.style.display !== "none") {
+    wrapper.style.display = "none";
+  }
 }
 
 
 // This will remove return links from footnote content.
 function removeReturnLinks() {
-    let fnReturn = document.getElementsByClassName("footnote-return"); 
-    while (fnReturn.length > 0) {
-        fnReturn[0].parentNode.removeChild(fnReturn[0]);
-    };
+  let fnReturn = document.getElementsByClassName("footnote-return"); 
+  while (fnReturn.length > 0) {
+    fnReturn[0].parentNode.removeChild(fnReturn[0]);
+  };
 }
 
 
 function footnotePopup() {
-    removeReturnLinks();
+  removeReturnLinks();
 
-    let refList = document.querySelectorAll("sup");
-    for (let x = 0; x < refList.length; x++) {
-        refList[x].addEventListener("click", function(event) {
-            event.preventDefault();    
-            // index.innerHTML = refList[x].id.substring(6,7) + ".";            
-            content.innerHTML = getFootnote(refList[x].id.substring(6,7));
-            wrapper.style.display = "flex";
+  let refList = document.querySelectorAll("sup");
+  for (let x = 0; x < refList.length; x++) {
+    refList[x].addEventListener("click", function(event) {
+      event.preventDefault();    
+      content.innerHTML = getFootnote(refList[x].innerText);
+      wrapper.style.display = "flex";
 
-        });
-    };
-
-    wrapper.addEventListener("click", function(event) {
-        closeFootnotePopup();
     });
+  };
+
+  wrapper.addEventListener("click", function(event) {
+    closeFootnotePopup();
+  });
 
 
-    window.addEventListener("scroll", function(event) {
-        closeFootnotePopup();
-    });
+  window.addEventListener("scroll", function(event) {
+    closeFootnotePopup();
+  });
 };
 
 footnotePopup();
