c6f15654e4a4fb555a18224fdfff486fc6963bb8
hoonti06 <hoonti06@gmail.com>
2020-01-31 16:06:52 +0900

    [system] test script 내부에 명시되어 있는 jekyll-pandoc-mermaid 버전 수정

--- excute-jekyll-pandoc-mermaid-test-environment.sh
+++ excute-jekyll-pandoc-mermaid-test-environment.sh
@@ -2,6 +2,6 @@
 
 cd jekyll-pandoc-mermaid
 gem build jekyll-pandoc-mermaid.gemspec
-gem install jekyll-pandoc-mermaid-0.0.1.gem
+gem install jekyll-pandoc-mermaid-1.0.2.gem
 cd ..
 bundle exec jekyll serve --trace
