0178bbfb8b1d15b9c19340258d082c903945c98e
hoonti06 <hoonti06@gmail.com>
2020-02-14 12:47:31 +0900

    [system] Docker image 변경

--- run_Docker-Jekyll.sh
+++ run_Docker-Jekyll.sh
@@ -1,12 +1,13 @@
 INSTANCE_NAME="blog"
 WORKING_DIR=/srv/jekyll
+DOCKER_IMAGE=hoonti06/hoonti06.gitlab.io-env
 
 docker rm $INSTANCE_NAME
 
 docker run -it --name $INSTANCE_NAME \
 	--volume="$PWD:/srv/jekyll" \
 	--network host \
-	hoonti06/ruby-pandoc-node /bin/bash -c '\
+	$DOCKER_IMAGE /bin/bash -c '\
 	cd /srv/jekyll; \
 	bundle install; \
 	bundle exec jekyll serve --trace; \
