0e257b2b9f12bee296cad6ab4004da63ad4e7d81
hoon <hoonti06@gmail.com>
2020-07-06 00:38:09 +0900

    [system] jekyll serve에 H 옵션 추가

--- excute-jekyll-pandoc-mermaid-test-environment.sh
+++ excute-jekyll-pandoc-mermaid-test-environment.sh
@@ -4,4 +4,4 @@ cd jekyll-pandoc-mermaid
 gem build jekyll-pandoc-mermaid.gemspec
 gem install jekyll-pandoc-mermaid-1.0.2.gem
 cd ..
-bundle exec jekyll serve --trace
+bundle exec jekyll serve -H 0.0.0.0 --trace
