1c11e3d8b3de29cbb200d3e1b07f10c01576725a
hoonti06 <hoonti06@gmail.com>
2020-07-06 10:00:21 +0900

    [system] replace-html.js 사용 중지 (close #28)

--- js/replace-html.js
+++ js/replace-html.js
@@ -2,3 +2,12 @@
   bodyHtml = document.body.innerHTML;
   document.body.innerHTML = bodyHtml.replace(/\u2013|\u2014/g, "--");
 })();
+
+$('p').each(function() {
+  var text = $(this).text();
+  $(this).text(text.replace(/\u2013|\u2014/g, '--'));
+});
+
+$('li').text(function() {
+  return $(this).text().replace(/\u2013|\u2014/g, '--');
+});
