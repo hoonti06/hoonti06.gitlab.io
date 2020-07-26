9f82c61a025bd3f89090790fbd972d3388a8aa0a
hoon <hoonti06@gmail.com>
2020-07-27 01:50:14 +0900

    [system] plantuml이 최대 사이즈를 안 넘도록 크키 조절(close #31)

---
+++ js/plantuml.js
@@ -0,0 +1,18 @@
+function resizePlantuml() {
+  let plantumlList = $('object.plantuml')
+  jQuery.each( $(plantumlList), function(i, plantuml) {
+
+    let maxWidth = $(plantuml).parent().width();
+
+    plantumlWidth = $(plantuml).width();
+
+    if (maxWidth < plantumlWidth)
+    {
+      $(plantuml).css("height", ($(plantuml).height() * maxWidth / plantumlWidth) + "px");
+      $(plantuml).css("width", maxWidth + "px");
+    }
+  });
+
+}
+
+window.onload = resizePlantuml;
