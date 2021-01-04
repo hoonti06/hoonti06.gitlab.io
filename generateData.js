a290aff81da3585872f32dd64fcbbe883f0a5248
hoon <hoonti06@gmail.com>
2021-01-05 00:47:39 +0900

    [system] 태그 화면이 동적으로 링크를 생성하도록 수정

--- generateData.js
+++ generateData.js
@@ -33,9 +33,6 @@ dataList.forEach(function collectTagMap(data) {
     if (!data.tag) {
         return;
     }
-    else if (data.published == 'false') {
-        return;
-    }
     data.tag.forEach(function(tag) {
         if (!tagMap[tag]) {
             tagMap[tag] = [];
@@ -54,20 +51,10 @@ for (tag in tagMap) {
 }
 saveTagMap(tagMap);
 
-
-const tagList = Object.keys(tagMap).sort(function sortByTagName(a, b) {
-    return a.toLowerCase().localeCompare(b.toLowerCase());
-});
-
-saveTagList(tagList)
-
 const pageMap = {};
 dataList.sort(function(a, b) {
     return a.url.toLowerCase().localeCompare(b.url.toLowerCase());
 }).forEach(function(page) {
-
-    if (page.published == 'false')
-      return;
 
     pageMap[page.fileName] = {
         type: page.type,
@@ -94,6 +81,10 @@ dataList.forEach(function(page) {
 
 savePageList(pageMap);
 
+saveTagFiles(tagMap, pageMap);
+
+saveTagCount(tagMap);
+
 function saveTagMap(tagMap) {
     fs.writeFile("./_data/tagMap.yml", YAML.stringify(tagMap), function(err) {
         if (err) {
@@ -103,12 +94,96 @@ function saveTagMap(tagMap) {
     });
 }
 
-function saveTagList(tagList) {
-    fs.writeFile("./_data/tagList.yml", YAML.stringify(tagList), function(err) {
+/**
+ * tag 하나의 정보 파일을 만든다.
+ * 각 태그 하나는 하나의 json 파일을 갖게 된다.
+ * 예를 들어 java 라는 태그가 있다면 ./data/tag/java.json 파일이 만들어진다.
+ * json 파일의 내용은 fileName과 collection으로 구성된다.
+ * 다음은 spring.json 파일의 예이다.
+ *
+{
+  "fileName": "spring",
+  "collection": {
+    "spring-boot": {
+      "type": "wiki",
+      "title": "(강의 내용 정리) Spring boot",
+      "summary": "",
+      "parent": "online-lecture",
+      "url": "/wiki/spring-boot",
+      "updated": "2021-01-04 00:45:54 +0900",
+      "children": []
+    },
+    "spring-framework-core": {
+      "type": "wiki",
+      "title": "(강의 내용 정리) 스프링 프레임워크 핵심 기술",
+      "summary": "",
+      "parent": "online-lecture",
+      "url": "/wiki/spring-framework-core",
+      "updated": "2021-01-04 00:45:48 +0900",
+      "children": []
+    },
+    "spring-mvc": {
+      "type": "wiki",
+      "title": "(강의 내용 정리) 스프링 웹 MVC",
+      "summary": "",
+      "parent": "online-lecture",
+      "url": "/wiki/spring-mvc",
+      "updated": "2021-01-04 00:46:04 +0900",
+      "children": []
+    }
+  }
+}
+ */
+function saveTagFiles(tagMap, pageMap) {
+    for (let tag in tagMap) {
+        var map = {
+            fileName: tag,
+            collection: {}
+        };
+        var tagData = tagMap[tag];
+        for (var i in tagData) {
+            var fileName = tagData[i].fileName;
+            map.collection[fileName] = pageMap[fileName]
+        }
+
+        if (!fs.existsSync("./data/tag")) {
+            fs.mkdirSync("./data/tag");
+        }
+
+        fs.writeFile("./data/tag/" + tag + ".json", JSON.stringify(map), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+}
+
+/**
+ * 태그 하나가 갖는 자식 문서의 수를 ./_data/tagCount.yml 파일로 저장한다.
+ * 만약 java 태그가 달린 문서가 4개 있고, spring 태그가 달린 문서가 5개 있다면 tagCount.yml 파일은 다음과 같은 내용을 갖게 된다.
+-
+    name: java
+    size: 4
+-
+    name: spring
+    size: 3
+ */
+function saveTagCount(tagMap) {
+    var list = [];
+    for (var tag in tagMap) {
+        list.push({
+            name: tag,
+            size: tagMap[tag].length
+        });
+    }
+    var sortedList = list.sort((a, b) =>
+        a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+
+    fs.writeFile("./_data/tagCount.yml", YAML.stringify(sortedList), function(err) {
         if (err) {
             return console.log(err);
         }
-        console.log("tagList saved.");
+        console.log("tagCount saved.");
     });
 }
 
