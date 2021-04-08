9b466c739943797f7bb1c5dbcc8a9b4643c1193c
hoon <hoonti06@gmail.com>
2021-04-08 10:25:55 +0900

    [system] footnote의 id를 문자열로도 설정 가능하도록 수정 close #38

--- js/footnote-popup.js
+++ js/footnote-popup.js
@@ -1,27 +1,19 @@
 let wrapper     = document.getElementById("popup-wrapper"); 
 let content     = document.getElementById("popup-content"); 
 
-
 function gatherFootnotes() {
-    // Counts generated footnotes. Hugo will create ordered list at the end of each post 
-    // with each footnote as item.
-    let fnCount = document.getElementById("fn:1").parentNode.children.length;
-
-    // Add every footnote into fnList ordered by their index.
-    let fnList = []; 
-    for (let x=1; x <= fnCount; x++) {
-        let footnote = document.getElementById(`fn:${x}`);
-        fnList.push(footnote);
-    };
-
-    return fnList;
+  let footnote = document.querySelectorAll('li[id^="fn:"]');
+  return footnote;
 }
 
 let fnList = gatherFootnotes();
 
-
-function getFootnote(index) {
-    return fnList[index-1].innerHTML.trim();
+function getFootnote(id) {
+  for (let i = 0; i < fnList.length; i++) {
+    // fnList[i].id => 'fn:id'
+    if (fnList[i].id.split(':')[1] == id)
+      return fnList[i].innerHTML.trim();
+  }
 };
 
 
@@ -34,7 +26,7 @@ function closeFootnotePopup() {
 
 // This will remove return links from footnote content.
 function removeReturnLinks() {
-    let fnReturn = document.getElementsByClassName("footnote-return"); 
+    let fnReturn = document.getElementsByClassName("footnote-return");
     while (fnReturn.length > 0) {
         fnReturn[0].parentNode.removeChild(fnReturn[0]);
     };
@@ -45,11 +37,11 @@ function footnotePopup() {
     removeReturnLinks();
 
     let refList = document.querySelectorAll("sup");
-    for (let x = 0; x < refList.length; x++) {
-        refList[x].addEventListener("click", function(event) {
-            event.preventDefault();    
-            // index.innerHTML = refList[x].id.substring(6,7) + ".";            
-            content.innerHTML = getFootnote(refList[x].id.substring(6,7));
+    for (let i = 0; i < refList.length; i++) {
+        refList[i].addEventListener("click", function(event) {
+            event.preventDefault();
+            // refList[i].id => 'sup:fnref:id'
+            content.innerHTML = getFootnote(refList[i].id.split(':')[1]);
             wrapper.style.display = "flex";
 
         });
